75454ce3f0710ce964acd73471817655caa0c4a2
Donovan <dlbm3@hotmail.com>
2015-12-26 14:33:37 -0800

    Moving Java to front of path.

--- scripts/SetupJava.ps1
+++ scripts/SetupJava.ps1
@@ -202,7 +202,7 @@ try
         if($env:Path.Contains("JAVA_HOME") -eq $false)
         {
             Write-Host "Adding JAVA_HOME to path"
-            [System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";%JAVA_HOME%\bin;%M2%", "Machine")
+            [System.Environment]::SetEnvironmentVariable("PATH", "%JAVA_HOME%\bin;%M2%;" + $Env:Path, "Machine")
         }
         else
         {
